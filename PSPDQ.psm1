6806d3c5f430a77b1b05612016de1b4816657614
Bayliss <cbaylissmk2@gmail.com>
2019-08-06 10:18:20 +0100

    Updated module files and created function to get config

--- PSPDQ.psm1
+++ PSPDQ.psm1
@@ -1,8 +1,9 @@
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue -Recurse )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Recurse )
 
 #Dot source the files
-Foreach ($import in $Public) {
+Foreach ($import in @($Public + $Private)) {
     Try {
         . $import.fullname
     } Catch {
